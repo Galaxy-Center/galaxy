73faa3267768c95ff238025b27a1fea40a026b8c
Lance <lancexu.hello@gmail.com>
2020-09-16 00:04:24 +0800

    filled funcs for task.go

--- gorm_models/task.go
+++ gorm_models/task.go
@@ -2,8 +2,9 @@ package models
 
 import (
 	"time"
+
+	galaxyDB "github.com/galaxy-center/galaxy/lifecycle"
 	"gorm.io/gorm"
-	galaxy_db "github.com/galaxy-center/galaxy/lifecycle"
 )
 
 // Task is an object representing the database table.
@@ -20,7 +21,7 @@ type Task struct {
 	SchedulingCategory string `gorm:"column:scheduling_category" json:"scheduling_category" toml:"scheduling_category" yaml:"scheduling_category"`
 	Assess             string `gorm:"column:assess" json:"assess" toml:"assess" yaml:"assess"`
 	Executor           string `gorm:"column:executor" json:"executor" toml:"executor" yaml:"executor"`
-	Active             bool   `gorm:"column:active" json:"active" toml:"active" yaml:"active"`
+	Actived            bool   `gorm:"column:actived" json:"active" toml:"active" yaml:"active"`
 	CreatedAt          uint64 `gorm:"autoCreateTime:nano" json:"created_at" toml:"created_at" yaml:"created_at"`
 	CreatedBy          string `gorm:"column:created_by" json:"created_by,omitempty" toml:"created_by" yaml:"created_by,omitempty"`
 	UpdatedAt          uint64 `gorm:"autoUpdateTime:nano" json:"updated_at" toml:"updated_at" yaml:"updated_at"`
@@ -75,29 +76,91 @@ func (Task) TableName() string {
 	return "task"
 }
 
+// BeforeCreate do somethings, e.g. checks for the necessary fileds.
 func (t *Task) BeforeCreate(tx *gorm.DB) (err error) {
-	currTime := time.Now().UnixNano()
-	if	t.CreatedAt <= 0 {
+	currTime := uint64(time.Now().UnixNano())
+	if t.CreatedAt <= 0 {
 		t.CreatedAt = currTime
 	}
 	if t.UpdatedAt <= 0 {
 		t.UpdatedAt = currTime
 	}
+	return nil
+}
+
+// AfterCreate do somethings, e.g. debug log.
+func (t *Task) AfterCreate(tx *gorm.DB) (err error) {
+	// nothing doing
+	return
 }
 
 // Create a single Task to db by *gorm.DB
 func Create(task *Task) error {
-	db := galaxy_db.GetDB()
+	db := galaxyDB.GetDB()
 	err := db.Create(task).Error
 	return err
+}
+
+// BeforeUpdate do somethings, e.g. updating the updated_at value.
+func (t *Task) BeforeUpdate(tx *gorm.DB) (err error) {
+	t.UpdatedAt = uint64(time.Now().UnixNano())
+	return
+}
+
+// AfterUpdate do somethings, e.g. update other database in the same transcation.
+// 在同一个事务中更新数据
+func (t *Task) AfterUpdate(tx *gorm.DB) (err error) {
+	// nothing doing
+	return
 }
 
 // Save a single task will be stored to db
 // Note: all the fields will be updated to db, includes default value.
 func Save(task *Task) error {
-	db := galaxy_db.GetDB()
-	if err := db.Save(task).Error; err != nil {
-		return err
+	db := galaxyDB.GetDB()
+	err := db.Save(task).Error
+	return err
+}
+
+// Updates updates from specific task that will not updating the zero value
+// fileds to db.
+// 只能保存非零字段
+func Updates(task *Task) error {
+	db := galaxyDB.GetDB()
+	err := db.Model(task).Updates(task).Error
+	return err
+}
+
+// UpdatesFromMap updates from specific task that will not updating the zero value
+// fileds to db.
+// 只能保存非零字段
+func UpdatesFromMap(id uint64, values map[string]interface{}) error {
+	db := galaxyDB.GetDB()
+	err := db.Model(&Task{}).Where("id = ?", id).Updates(values).Error
+	return err
+}
+
+// Delete delete permanently. 永久删除
+func Delete(id uint64) error {
+	db := galaxyDB.GetDB()
+	err := db.Where(&Task{}, id).Error
+	return err
+}
+
+// DeleteByActived delete softly. 软删除
+func DeleteByActived(id uint64) error {
+	db := galaxyDB.GetDB()
+	err := db.Model(&Task{}).Where("id = ?", id).Update("actived", false).Error
+	return err
+}
+
+// Get returns the task by specific id.
+func Get(id uint64) (*Task, error) {
+	db := galaxyDB.GetDB()
+	var task Task
+	err := db.First(&task, id).Error
+	if err != nil {
+		return &task, nil
 	}
-	retun nil
+	return nil, err
 }
